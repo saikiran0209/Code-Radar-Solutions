bdfc2fefe5534dc7737f0d4a487233ea6734dc6a
Sai Kiran Sharma <saikiran.2.9.2005@gmail.com>
2025-02-12 20:30:33 +0530

    Solution to Determine Eligibility for Voting

---
+++ Determine Eligibility for Voting@@6788dd63490637cfc44e1b99/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int age;
+    scanf("%d",&age);
+    if(age>=18){
+        printf("Eligible");
+    }else{
+        printf("Not Eligible");
+    }
+    return 0;
+}
